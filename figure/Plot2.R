73386c465c7493afa40fee0116a2ecb367c60900
cm1791 <31619696+cm1791@users.noreply.github.com>
2017-09-07 11:47:12 +0200

    Delete Plot2.R

--- figure/Plot2.R
+++
@@ -1,5 +0,0 @@
-#Plot 2
-
-plot(df.subset$Global_active_power~df.subset$dateTime, type="l", ylab="Global Active Power (kilowatts)", xlab="")
-dev.copy(png, "plot2.png", width=480, height=480)
-dev.off()
