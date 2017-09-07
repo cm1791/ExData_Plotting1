669b82efeb0753d08d7099a01da724699468498f
cm1791 <31619696+cm1791@users.noreply.github.com>
2017-09-07 11:47:04 +0200

    Delete Plot1.R

--- figure/Plot1.R
+++
@@ -1,5 +0,0 @@
-#Plot 1
-
-hist(df.subset$Global_active_power, col = "red", main = paste("Global Active Power"), xlab = "Global Active Power (kilowatts)")
-dev.copy(png, "plot1.png", width=480, height=480)
-dev.off()
